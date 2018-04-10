680356c2348ace103f1ca565e1df718ce352b0dd
AVPolyakov <alexandr.polyakov@gmail.com>
2018-04-11 01:37:50 +0300

    Add ShowException

--- SharpLayout.Tests/Program.cs
+++ SharpLayout.Tests/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using PdfSharp.Drawing;
 
 namespace SharpLayout.Tests
 {
@@ -7,22 +8,39 @@ namespace SharpLayout.Tests
     {
         static void Main()
         {
-            var document = new Document {
-                //CellsAreHighlighted = true,
-                //R1C1AreVisible = true,
-                //ParagraphsAreHighlighted = true,
-                //CellLineNumbersAreVisible = true,
-                //ExpressionVisible = true,
-            };
-            PaymentOrder.AddSection(document, new PaymentData{IncomingDate = DateTime.Now, OutcomingDate = DateTime.Now});
-            //Svo.AddSection(document);
-            //ContractDealPassport.AddSection(document);
-            //LoanAgreementDealPassport.AddSection(document);
+            try
+            {
+                var document = new Document {
+                    //CellsAreHighlighted = true,
+                    //R1C1AreVisible = true,
+                    //ParagraphsAreHighlighted = true,
+                    //CellLineNumbersAreVisible = true,
+                    //ExpressionVisible = true,
+                };
+                PaymentOrder.AddSection(document, new PaymentData {IncomingDate = DateTime.Now, OutcomingDate = DateTime.Now});
+                //Svo.AddSection(document);
+                //ContractDealPassport.AddSection(document);
+                //LoanAgreementDealPassport.AddSection(document);
 
+                document.SavePng(0, "Temp.png", 120).StartLiveViewer(true);
+
+                //Process.Start(document.SavePng(0, "Temp2.png")); //open with Paint.NET
+                //Process.Start(document.SavePdf($"Temp_{Guid.NewGuid():N}.pdf"));
+            }
+            catch (Exception e)
+            {
+                ShowException(e);
+            }
+        }
+
+        private static void ShowException(Exception e)
+        {
+            var document = new Document();
+            var settings = new PageSettings();
+            settings.LeftMargin = settings.TopMargin = settings.RightMargin = settings.BottomMargin = Util.Cm(0.5);
+            document.Add(new Section(settings).Add(new Paragraph()
+                .Add($"{e}", new XFont("Consolas", 9.5))));
             document.SavePng(0, "Temp.png", 120).StartLiveViewer(true);
-
-            //Process.Start(document.SavePng(0, "Temp2.png")); //open with Paint.NET
-            //Process.Start(document.SavePdf($"Temp_{Guid.NewGuid():N}.pdf"));
         }
 
         public static void StartLiveViewer(this string fileName, bool alwaysShowWindow)
