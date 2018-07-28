861e4e4f6c519d774869c5a0a54efe70902fba9c
AVPolyakov <alexandr.polyakov@gmail.com>
2018-07-28 14:15:34 +0300

    3000*17

--- SharpLayout.Tests/Program.cs
+++ SharpLayout.Tests/Program.cs
@@ -1,46 +1,73 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using PdfSharp;
 using PdfSharp.Drawing;
+using static SharpLayout.Direction;
+using static SharpLayout.Util;
 
 namespace SharpLayout.Tests
 {
     static class Program
     {
-        static void Main()
+        static async Task Main()
         {
-            Document.CollectCallerInfo = true;
-            try
+            M1(50);
+            for (var j = 1; j <= 17; j++)
             {
-                var document = new Document {
-                    //CellsAreHighlighted = true,
-                    //R1C1AreVisible = true,
-                    //ParagraphsAreHighlighted = true,
-                    //CellLineNumbersAreVisible = true,
-                    //ExpressionVisible = true,
-                };
-                PaymentOrder.AddSection(document, new PaymentData {IncomingDate = DateTime.Now, OutcomingDate = DateTime.Now});
-                //Svo.AddSection(document);
-                //ContractDealPassport.AddSection(document);
-                //LoanAgreementDealPassport.AddSection(document);
-
-                document.SavePng(0, "Temp.png", 120).StartLiveViewer(true);
-
-                //Process.Start(document.SavePng(0, "Temp2.png")); //open with Paint.NET
-                //Process.Start(document.SavePdf($"Temp_{Guid.NewGuid():N}.pdf"));
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+                await Task.WhenAll(Enumerable.Range(1, j).Select(i => Task.Run(() => {
+                    //
+                    M1(3000);
+                })));
+                stopwatch.Stop();
+                Console.WriteLine($"{j} {stopwatch.ElapsedMilliseconds}");
             }
-            catch (Exception e)
+
+            //var path = $"Temp_{Guid.NewGuid():N}.pdf";
+            //File.WriteAllBytes(path, bytes);
+            //Process.Start(path);
+        }
+
+        private static byte[] M1(int count)
+        {
+            var document = new Document();
+            var section = document.Add(new Section(new PageSettings {
+                TopMargin = Cm(1.2),
+                BottomMargin = Cm(1),
+                LeftMargin = Cm(2),
+                RightMargin = Cm(1),
+                Orientation = PageOrientation.Landscape
+            }));
+            var table = section.AddTable().Border(Styles.BorderWidth).Font(Styles.TimesNewRoman8);
+            var columnCount = 14;
+            for (var i = 0; i < columnCount; i++)
+                table.AddColumn();
+            table.Columns.ToArray().Distribute(section.PageSettings.PageWidthWithoutMargins);
+            for (var j = 0; j < count; j++)
             {
-                ShowException(e);
+                table.AddRow(r => {
+                    for (var i = 0; i < columnCount; i++)
+                    {
+                        r[table.Columns[i]].Add(new Paragraph().Margin(Left | Right, Cm(0.05))
+                            .Add("QWEQWE QWETQWTQWRTWERT QWETQWET QWEQWR ASDASF"));
+                    }
+                });
             }
+
+            var bytes = document.CreatePdf();
+            return bytes;
         }
 
         private static void ShowException(Exception e)
         {
             var document = new Document();
             var settings = new PageSettings();
-            settings.LeftMargin = settings.TopMargin = settings.RightMargin = settings.BottomMargin = Util.Cm(0.5);
+            settings.LeftMargin = settings.TopMargin = settings.RightMargin = settings.BottomMargin = Cm(0.5);
             document.Add(new Section(settings).Add(new Paragraph()
                 .Add($"{e}", new XFont("Consolas", 9.5))));
             document.SavePng(0, "Temp.png", 120).StartLiveViewer(true, false);
