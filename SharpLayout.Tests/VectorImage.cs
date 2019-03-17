10ba85d05dc2af48f60954f0c8364e02e55444b8
AVPolyakov <alexandr.polyakov@gmail.com>
2019-03-17 14:11:45 +0300

    dev

---
+++ SharpLayout.Tests/VectorImage.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using PdfSharp.Drawing;
+using static SharpLayout.Direction;
+using static SharpLayout.Tests.Styles;
+using static SharpLayout.Util;
+
+namespace SharpLayout.Tests
+{
+    public static class VectorImage
+    {
+        public static void AddSection(Document document)
+        {
+            var section = document.Add(new Section(new PageSettings()));
+            var info = VectorImageContent.ImageInfo.Value;
+            {
+                var table = section.AddTable();
+                var scale = 0.7;
+                var c1 = table.AddColumn(info.PointWidth * scale);
+                table.AddColumn(Px(500));
+                table.AddColumn(Px(500));
+                var r1 = table.AddRow();
+                r1[c1].Add(new Image()
+                    .Width(info.PointWidth * scale).Height(info.PointHeight * scale)
+                    .Content(new VectorImageContent()));
+            }
+            {
+                var table = section.AddTable().Margin(Top, Cm(0.5));
+                var scale = 0.3;
+                var c1 = table.AddColumn();
+                var c2 = table.AddColumn();
+                var c3 = table.AddColumn();
+                table.Columns.ToArray().Distribute(section.PageSettings.PageWidthWithoutMargins - BorderWidth);
+                var r1 = table.AddRow();
+                r1[c1].Add(new Image()
+                    .Width(info.PointWidth * scale).Height(info.PointHeight * scale)
+                    .Content(new VectorImageContent()));
+                var r2 = table.AddRow();
+                r2[c2].Add(new Image().Alignment(HorizontalAlign.Center)
+                    .Width(info.PointWidth * scale).Height(info.PointHeight * scale)
+                    .Content(new VectorImageContent()));
+                var r3 = table.AddRow();
+                r3[c3].Add(new Image().Alignment(HorizontalAlign.Right)
+                    .Width(info.PointWidth * scale).Height(info.PointHeight * scale)
+                    .Content(new VectorImageContent()));
+            }
+        }
+
+        public class VectorImageContent : IImageContent
+        {
+            public static readonly Lazy<ImageInfo> ImageInfo = new Lazy<ImageInfo>(() => {
+                var bytes = File.ReadAllBytes(@"Images\blue-rabbit.pdf");
+                using (var stream = new MemoryStream(bytes))
+                using (var xImage = XImage.FromStream(stream))
+                    return new ImageInfo(bytes, xImage.PointWidth, xImage.PointHeight);
+            });
+
+            public Stream CreateStream() => new MemoryStream(ImageInfo.Value.Bytes);
+        }
+
+    }
+
+    public class ImageInfo
+    {
+        public byte[] Bytes { get; }
+        public double PointWidth { get; }
+        public double PointHeight { get; }
+
+        public ImageInfo(byte[] bytes, double pointWidth, double pointHeight)
+        {
+            Bytes = bytes;
+            PointWidth = pointWidth;
+            PointHeight = pointHeight;
+        }
+    }
+}
