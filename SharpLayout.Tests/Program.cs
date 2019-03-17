c13efba480f8b1cb16bd7e8cbbe52f02b6bad951
AVPolyakov <alexandr.polyakov@gmail.com>
2019-03-17 15:22:50 +0300

    dev

--- SharpLayout.Tests/Program.cs
+++ SharpLayout.Tests/Program.cs
@@ -7,7 +7,10 @@ namespace SharpLayout.Tests
         static void Main()
         {
             var document = new Document();
-            PaymentOrder.AddSection(document, new PaymentData {IncomingDate = DateTime.Now, OutcomingDate = DateTime.Now});
+            PaymentOrder.AddSection(document, new PaymentData {
+                IncomingDate = DateTime.Now,
+                OutcomingDate = DateTime.Now
+            });
             document.SavePdf($"Temp_{Guid.NewGuid():N}.pdf");
         }
     }
