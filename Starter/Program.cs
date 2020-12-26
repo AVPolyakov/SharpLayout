2faa5228916460ab3eee3aa3fc166094e56b270a
AVPolyakov <alexandr.polyakov@gmail.com>
2020-12-26 10:20:01 +0300

    Migrate to .NET 5

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -82,7 +82,7 @@ namespace Starter
         }
 
         private static string Ide => Environment.UserName switch {
-            "APolyakov" => "vs",
+            "APolyakov" => "rider",
             _ => "vs"
         };
 
