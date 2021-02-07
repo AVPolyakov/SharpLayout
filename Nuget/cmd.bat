4b70e159aa2f1c24b62feb105e9eb7a97598d5b4
AVPolyakov <alexandr.polyakov@gmail.com>
2021-02-07 20:23:00 +0300

    Update .nuspec

---
+++ Nuget/cmd.bat
@@ -0,0 +1,11 @@
+copy SharpLayout.nuspec ..\SharpLayout\
+NuGet.exe pack ..\SharpLayout\SharpLayout.csproj -IncludeReferencedProjects -Prop Configuration=Release
+del ..\SharpLayout\SharpLayout.nuspec
+
+copy SharpLayout.ImageRendering.nuspec ..\SharpLayout.ImageRendering\
+NuGet.exe pack ..\SharpLayout.ImageRendering\SharpLayout.ImageRendering.csproj -Prop Configuration=Release
+del ..\SharpLayout.ImageRendering\SharpLayout.ImageRendering.nuspec
+
+copy SharpLayout.WatcherCore.nuspec ..\SharpLayout.WatcherCore\
+NuGet.exe pack ..\SharpLayout.WatcherCore\SharpLayout.WatcherCore.csproj -Prop Configuration=Release
+del ..\SharpLayout.WatcherCore\SharpLayout.WatcherCore.nuspec
